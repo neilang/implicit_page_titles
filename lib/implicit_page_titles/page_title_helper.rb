9c6c324ca91e37e1917d09f2255574dfdbd3c660
neilang <neilang@gmail.com>
2015-04-11 14:20:22 +1000

    Added a default page_title response

--- lib/implicit_page_titles/page_title_helper.rb
+++ lib/implicit_page_titles/page_title_helper.rb
@@ -1,5 +1,9 @@
 module ImplicitPageTitles
   module PageTitleHelper
+    def page_title
+      app_name
+    end
+
     def app_name
       Rails.application.class.to_s.split("::").first.underscore.humanize
     end
--- spec/helpers/page_title_helper_spec.rb
+++ spec/helpers/page_title_helper_spec.rb
@@ -1,6 +1,11 @@
 require "rails_helper"
 
 describe ImplicitPageTitles::PageTitleHelper, type: :helper do
+  describe "#page_title default response" do
+    subject { helper.page_title }
+    it { should == "Dummy app" }
+  end
+
   describe "#app_name" do
     subject { helper.app_name }
     it { should == "Dummy app" }
