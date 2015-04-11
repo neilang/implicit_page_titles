825dc1b3f7e6a5e9707e527741c179e10245655e
neilang <neilang@gmail.com>
2015-04-11 16:01:22 +1000

    Added convention to look for main objects title attribute

--- spec/helpers/page_title_helper_spec.rb
+++ spec/helpers/page_title_helper_spec.rb
@@ -32,6 +32,37 @@ describe ImplicitPageTitles::PageTitleHelper, type: :helper do
     end
   end
 
+  describe "#show_action_page_title" do
+    before do
+      allow(helper).to receive(:action_name).and_return("show")
+      allow(helper).to receive(:controller_name).and_return("posts")
+    end
+    it "returns the main object title" do
+      controller.instance_variable_set :@post, double("post", title: "Hello")
+      expect(helper.show_action_page_title).to eq "Hello"
+    end
+
+    it "returns the main object name if there is no title" do
+      controller.instance_variable_set :@post, double("post", name: "World")
+      expect(helper.show_action_page_title).to eq "World"
+    end
+
+    it "returns the main object title over name" do
+      controller.instance_variable_set :@post, double("post", title: "Hello", name: "World")
+      expect(helper.show_action_page_title).to eq "Hello"
+    end
+
+    it "returns nil if no title or name attribute" do
+      controller.instance_variable_set :@post, double("post")
+      expect(helper.show_action_page_title).to eq nil
+    end
+
+    it "returns nil when not on show" do
+      allow(helper).to receive(:action_name).and_return("index")
+      expect(helper.show_action_page_title).to eq nil
+    end
+  end
+
   describe "#path_to_page_title" do
     it "works with a simple path" do
       expect(helper.path_to_page_title("/test")).to eq "Test"
